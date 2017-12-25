c5d84e4ebbfd5a6e8ae51596b14d080d33e5013d
trilliean <648270560@qq.com>
2017-12-25 10:52:25 +0800

    添加了输出爬取到的数据添加到文件的功能

--- crawler/qiushibaikeSpider.py
+++ crawler/qiushibaikeSpider.py
@@ -1,3 +1,5 @@
+#encoding:
+
 import requests
 import re
 base_url = "https://qiushibaike.com/text/page/1"
@@ -7,21 +9,8 @@ text_pattern = "<span>\n\n([\s\S]*?)\n</span>"
 author_pattern = "<h2>\n([\s\S]*?)\n</h2>"
 maxpage_pattern = "<span class=\"page-numbers\">([\s\S]*?)</span>"
 
-qiubai_patterns = {
-    "root_pattern":"<div class=\"author clearfix\">([\s\S]*?)<div class=\"stats\">",
-    "maxpage_pattern":" <span class=\"page-numbers\">([\s\S]*?)</span>",
-    "text_pattern":"<span>\n\n([\s\S]*?)\n</span>",
-    "author_pattern":"<h2>\n([\s\S]*?)\n</h2>"
-}
 r = requests.get(base_url,headers=headers)
 base_str = r.text
-root_texts = re.findall(root_pattern, base_str)
-for text in root_texts:
-    author = re.findall(author_pattern, text)
-    if author==[]:
-        author = ['佚名']
-    content = re.findall(text_pattern, text)
-    #print("作者：%s type:%s \n 内容: %s" % (author, type(author), content))
 
 # 获取各页面的内容
 def get_contents():
@@ -32,24 +21,15 @@ def get_contents():
         str = resp.text
         root_contents = re.findall(root_pattern, str)
         print("-------------------------------第%s页:-------------------------" % (i + 1))
+        with open(r'E:\test\qiushibaike.txt', 'a') as f:
+            f.write('-------------------------------第%s页:-------------------------\n' % (i + 1))
         for content in root_contents:
             author = re.findall(author_pattern, content)
             if(author == []):
                 author = ['佚名']
             text = re.findall(text_pattern, content)
-            print("作者：%s" % author)
-            print("内容：%s" % text)
+            with open(r'E:\test\qiushibaike.txt', 'a', encoding='gbk', errors='backslashreplace') as f:
+                f.write('作者: %s\n' % author)
+                f.write('内容: %s\n' % text[0][1:-1])
 
 get_contents()
-
-# 提取各页面的数据
-# @str root_contents 该页面的所有内容
-# @dict 提取的规则。例：{"a_pattern":"pattern_contents"}
-def analysis_contents(root_contents, *patterns):
-    patternDict = patterns[0]
-    for (k, v) in patternDict.items():
-        pass
-
-
-#get_contents()
-#analysis_contents(base_str, qiubai_patterns)
