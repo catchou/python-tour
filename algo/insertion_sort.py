22fb52d5a43b3ff88defc37499de643d1629a8e4
trilliean <648270560@qq.com>
2017-12-27 16:50:02 +0800

    修改了插入排序的代码

--- algo/insertion_sort.py
+++ algo/insertion_sort.py
@@ -5,5 +5,14 @@ def swap(list, i, j):
     list[j] = temp
 
 def insertionSort(list, n):
-    for i in range(1,n):
-        pass
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if list[j] < list[j - 1]:
+                swap(list, j, j - 1)
+
+test = [3, 5, 2, 1, 7, 4, 9]
+print(test)
+insertionSort(test, 7)
+print(test)
+
+
